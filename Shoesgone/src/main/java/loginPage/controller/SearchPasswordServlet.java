093b721de209011d238e5ddb76881cf5c1da2b83
JeongHyunBaek <bjhbhm@gmail.com>
2021-07-28 22:16:12 +0900

    Shoesgone Master

--- Shoesgone/src/main/java/loginPage/controller/SearchPasswordServlet.java
+++ Shoesgone/src/main/java/loginPage/controller/SearchPasswordServlet.java
@@ -109,7 +109,7 @@ public class SearchPasswordServlet extends HttpServlet {
 		char[] charSet = new char[] { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F',
 				'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a',
 				'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
-				'w', 'x', 'y', 'z', '!', '@', '#', '$', '%', '^', '&' };
+				'w', 'x', 'y', 'z', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '+' };
 		StringBuffer sb = new StringBuffer();
 		SecureRandom sr = new SecureRandom();
 		sr.setSeed(new Date().getTime());
